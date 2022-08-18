36e013235cf91a9393c6dfb6ee52c9de0fb6a4e3
ihorhorbunov <igorgorbunov3333@gmail.com>
2022-08-18 16:08:57 +0300

    [1.18.4] - Pause pomodoro only after minimum duration from config

--- src/main/java/com/igorgorbunov3333/timer/service/console/command/impl/PauseCommandProcessor.java
+++ src/main/java/com/igorgorbunov3333/timer/service/console/command/impl/PauseCommandProcessor.java
@@ -1,7 +1,9 @@
 package com.igorgorbunov3333.timer.service.console.command.impl;
 
+import com.igorgorbunov3333.timer.config.properties.PomodoroProperties;
 import com.igorgorbunov3333.timer.service.console.command.CommandProcessor;
 import com.igorgorbunov3333.timer.service.console.printer.util.SimplePrinter;
+import com.igorgorbunov3333.timer.service.pomodoro.engine.PomodoroEngine;
 import com.igorgorbunov3333.timer.service.pomodoro.engine.PomodoroEngineService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,20 @@ import org.springframework.stereotype.Service;
 public class PauseCommandProcessor implements CommandProcessor {
 
     private final PomodoroEngineService pomodoroEngineService;
+    private final PomodoroEngine pomodoroEngine;
+    private final PomodoroProperties pomodoroProperties;
 
     @Override
     public void process() {
+        long durationInSeconds = pomodoroEngine.getPomodoroCurrentDuration();
+
+        if (durationInSeconds <= pomodoroProperties.getDuration()) {
+            SimplePrinter.print(String.format("To pause a pomodoro, its duration must be greater than the specified minimum: [%d] seconds)",
+                    pomodoroProperties.getDuration()));
+
+            return;
+        }
+
         pomodoroEngineService.pausePomodoro();
         SimplePrinter.print(String.format("Pomodoro paused at %s", pomodoroEngineService.getPomodoroCurrentDuration()));
     }
