4d8173ffc83688c84b9f81ec9008e18de20fd7a4
ihorhorbunov <igorgorbunov3333@gmail.com>
2022-10-12 17:47:10 +0300

    [1.20.3] - Don't include today into the week report sent via telegram

--- src/main/java/com/igorgorbunov3333/timer/service/message/pomodoro/report/impl/WeekPomodoroStandardReportMessageProvider.java
+++ src/main/java/com/igorgorbunov3333/timer/service/message/pomodoro/report/impl/WeekPomodoroStandardReportMessageProvider.java
@@ -1,6 +1,7 @@
 package com.igorgorbunov3333.timer.service.message.pomodoro.report.impl;
 
 import com.igorgorbunov3333.timer.model.dto.PeriodDto;
+import com.igorgorbunov3333.timer.model.dto.pomodoro.PomodoroDto;
 import com.igorgorbunov3333.timer.model.dto.pomodoro.period.WeeklyPomodoroDto;
 import com.igorgorbunov3333.timer.model.dto.pomodoro.report.PomodoroStandardReportDto;
 import com.igorgorbunov3333.timer.service.exception.MessageProcessingException;
@@ -14,7 +15,9 @@ import org.springframework.util.CollectionUtils;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
@@ -46,13 +49,27 @@ public class WeekPomodoroStandardReportMessageProvider implements MessageProvide
     }
 
     private String buildReportMessage(WeeklyPomodoroDto weeklyPomodoroDto) {
-        PomodoroStandardReportDto report = pomodoroStandardReporter.report(weeklyPomodoroDto.getPeriod(), weeklyPomodoroDto.getPomodoro());
+        PeriodDto periodWithoutToday = getPeriodWithoutToday(weeklyPomodoroDto);
+        List<PomodoroDto> pomodoroWithoutToday = getPomodoroWithoutToday(weeklyPomodoroDto, periodWithoutToday.getEnd());
 
-        PeriodDto period = weeklyPomodoroDto.getPeriod();
+        PomodoroStandardReportDto report = pomodoroStandardReporter.report(periodWithoutToday, pomodoroWithoutToday);
 
-        String header = "Report for period: " + period.getStart().toLocalDate() + "  -  " + period.getEnd().toLocalDate() + "\n";
+        String header = "Report for: " + periodWithoutToday.getStart().toLocalDate() + "  -  " + periodWithoutToday.getEnd().toLocalDate() + "\n";
 
         return buildReportMessage(report, header);
+    }
+
+    private PeriodDto getPeriodWithoutToday(WeeklyPomodoroDto weeklyPomodoroDto) {
+        PeriodDto period = weeklyPomodoroDto.getPeriod();
+        return new PeriodDto(period.getStart(), period.getEnd().minusDays(1L));
+    }
+
+    private List<PomodoroDto> getPomodoroWithoutToday(WeeklyPomodoroDto weeklyPomodoroDto, LocalDateTime periodEnd) {
+        List<PomodoroDto> pomodoro = weeklyPomodoroDto.getPomodoro();
+
+        return pomodoro.stream()
+                .filter(p -> !p.getEndTime().toLocalDate().atStartOfDay().isAfter(periodEnd))
+                .collect(Collectors.toList());
     }
 
     private boolean isStartOfWeek(LocalDate today) {
